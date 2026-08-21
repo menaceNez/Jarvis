8ad1af9ca4cb165c31148bd92361ac2a3143f8ac
menaceNez <stankgav@gmail.com>
2026-08-20 20:42:59 -0500

    init - runnable with npm i -> npm run start

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Jarvis
+
 https://www.tiktok.com/discover/iron-man-jarvis-jork-it-a-lil 😹
+
+# For getting started with an electron app start here:
+
+- [https://www.electronjs.org/docs/latest/tutorial/tutorial-prerequisites](electron)
---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Hello from electron");
+
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  console.log("Window-all-close event triggered!");
+  if (process.platform !== "darwin") app.quit();
+});
